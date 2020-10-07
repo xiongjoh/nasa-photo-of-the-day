95a3a220832946364832b14208978fb3161df726
unknown <xiongjoh@gmail.com>
2020-10-07 15:51:31 -0500

    passed data as prop instead of indidual keys from object line30

--- src/components/NasaDetail.js
+++ src/components/NasaDetail.js
@@ -1,18 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 
 export default function NasaDetails(props) {
-    const {url, title, explanation, date, author} = props
-
-    if (!url) return <h3>Loading...</h3>
+    // const {url, title, explanation, date, author} = props
+    console.log(props.url)
+    // if (!url) return <h3>Loading...</h3>
+    if (!props.url) return <h3>Loading...</h3>
 
     return (
         <div className='container'>
-            <img src={url}/>
-            <h2>{title}</h2>
-            <h3>{author}</h3>
-            <p>{date}</p>
-            <p>{explanation}</p>
+            <img src={props.url}/>
+            <h2>{props.title}</h2>
+            <h3>{props.author}</h3>
+            <p>{props.date}</p>
+            <p>{props.explanation}</p>
         </div>
+        // <div className='container'>
+        //     <img src={url}/>
+        //     <h2>{title}</h2>
+        //     <h3>{author}</h3>
+        //     <p>{date}</p>
+        //     <p>{explanation}</p>
+        // </div>
     )
 }
