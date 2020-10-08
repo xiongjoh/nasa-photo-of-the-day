097f6f97563495ec89e45ed8c17bca2a2a654e49
unknown <xiongjoh@gmail.com>
2020-10-08 17:03:49 -0500

    add date selector

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import NasaDetail from "./components/NasaDetail"
 import NasaHeader from "./components/NasaHeader"
 import NasaFooter from "./components/NasaFooter"
+import DatePicker from 'react-datepicker'
+import months from './pageObjects/months'
 
 const myKey = '5WwmxoufDLmWvQcaVXlKGhEoncVjMVLu6iP0ddKn'
 const demoKey = 'DEMO_KEY'
@@ -12,29 +14,55 @@ const apodAPI = 'https://api.nasa.gov/planetary/apod?api_key='
 function App() {
 
 const [nasaData, setNasaData] = useState({})
-const [date, setDate] = useState(null)
+const [date, setDate] = useState(new Date())
+const [selectedDate, setSelectedDate] = useState('')
 
-const selectDate = () => {
-  
+const changeDate = () => {
+  let theDate = String(date).split(' ').slice(1,4).reverse()
+  theDate[2] = months[theDate[2].toLowerCase()]
+  theDate = [theDate[0],theDate[2],theDate[1]]
+  console.log('You selected the date ' + theDate.join('-'))
+  setSelectedDate(theDate.join('-'))
+
 }
 
 useEffect(() => {
 
   const fetchNasaData = () => {
-    axios.get(`${apodAPI}${myKey}`)
+    axios.get(`${apodAPI}${myKey}&date=${selectedDate}`)
     .then(res => {
       setNasaData(res.data)
       console.log(res.data)
     })
     .catch(err => {
+      console.log('unable to show future dates')
+      axios.get(`${apodAPI}${myKey}`)
+      .then(res => {
+        setNasaData(res.data)
+        console.log(res.data)
+      })
+      .catch(err => {
+        debugger
+      })
       debugger
     })
   }
 
   fetchNasaData()
-}, [])
+}, [selectedDate])
 
-useEffect(() => {}, [date])
+// const MyContainer = ({ className, children}) => {
+//   return (
+//     <div style={{ padding: '16px', background: '#216ba5', color:'#fff'}}>
+//       <CalendarContainer className={className}>
+//         <div style={{background:'#f0f0f0'}}>
+//           What is your favorite day?
+//         </div>
+//         <div style={{ position: 'relative'}}>{children}</div>
+//       </CalendarContainer>
+//     </div>
+//   )
+// }
 
   return (
     
@@ -43,7 +71,12 @@ useEffect(() => {}, [date])
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
-      <NasaHeader/>
+      <div>{'Select Your Date '}
+        <DatePicker selected={date} onChange={d => setDate(d)}/>
+        <button onClick={() => changeDate()}>Enter</button>
+      </div>
+      
+      <NasaHeader></NasaHeader>
       <NasaDetail url={nasaData.url} title={nasaData.title} explanation={nasaData.explanation} date={nasaData.date} author={nasaData.copyright}/>
       <NasaFooter hdURL={nasaData.hdurl}/>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { ThemeProvider } from 'styled-components'
 import theme from './themes'
+import 'react-datepicker/dist/react-datepicker.css'
 
 
 ReactDOM.render(<ThemeProvider theme={theme}><App /></ThemeProvider>, document.getElementById("root"));
